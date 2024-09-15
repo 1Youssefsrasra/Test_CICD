8f3e8a2ec3104400a881faea7f2b90feaea4d45d
Youssef Srasra <youssef.srasra10@gmail.com>
2024-09-15 10:43:54 +0100

    first commit

---
+++ Backend/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use("/api/test" , (_req, res) => {
+    res.send("it works !!")
+} )
+
+
+app.listen(PORT, ()=>{
+    console.log("server is running!")
+});
